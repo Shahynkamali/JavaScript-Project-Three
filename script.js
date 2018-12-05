bff84a88c0dc88cc93e60b13ac0a7acd25014b8b
Shahynkamali <shahynkamali@gmail.com>
2018-12-05 14:39:10 -0500

    added html content

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-$(document).ready(function() {
-  $('#searchRestaurant').on('keyup', function(e){
-  	let nameRestaurant = e.target.value;
 
-  			$.ajax({
-  				url:` https://opentable.herokuapp.com/api/restaurants?city=Toronto&name=${nameRestaurant}`,
-  				method: 'GET',
-  				dataType: 'json'
-  			}).done(function(restaurant){
-  				$.each(restaurant, function(index, place){
-  			     for(i = 0; i < place.length; i++){
-              console.log(place[0])
-              console.log("Hello");
+let restaurantNames = [];
 
-             }
-  				})
-  			})
+
+
+$('#submit').on("click", function(){
+	const getRestaurant = $('input[name=name]').val();
+	const endpoint = `http://opentable.herokuapp.com/api/restaurants?name=${getRestaurant}`;
+
+	fetch(endpoint)
+	.then(obj => obj.json())
+	.then(data => {
+
+  	const restaurantArray = data.restaurants;
+  	const resName = restaurantArray.map(value =>{	
+  	restaurantNames.push(value);
+  	   })  
+
+  	const filteredRestaurant = restaurantNames.forEach(restaurantobj =>{
+  	const restaurantHTML = `
+	<li><a href="${restaurantobj.mobile_reserve_url}" class="list-group-item">${restaurantobj.name}</a></li>`
+  	$('.result').append(restaurantHTML);
+		 })
   	
+	 })
+
+   });
+
+console.log(restaurantNames);
 
 
-	
-	})
-});
