acc74dba9f665240e41759ace6bb2d44bd02be64
Shahynkamali <shahynkamali@gmail.com>
2018-12-20 16:35:47 -0500

    fixed css and made a list

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-let restaurantNames = [];
-
-
-
 $('#submit').on("click", function(e){
 	e.preventDefault();
 	const getRestaurant = $('input[name=name]').val();
@@ -20,13 +16,14 @@ $('#submit').on("click", function(e){
 
   	const filteredRestaurant = restaurantNames.map(restaurantobj =>{
 
-  		console.log(restaurantobj)
+  		
   	const restaurantHTML = `
 	<div class = "listItems">
 		<li>
 		  <a href="${restaurantobj.mobile_reserve_url}" class="list-group-item">${restaurantobj.name}</a>
-		  <button type="submit" class="btn btn-default">Add to List!</button>
+		  <button type="submit" id="addItem" class="btn btn-default addItem">Add to List!</button>
 		</li>
+		<img src=${restaurantobj.image_url}>
 	</div>	`
 	return restaurantHTML
   
@@ -35,6 +32,46 @@ $('#submit').on("click", function(e){
 	 })
 
    });
+let restaurantList = [];
+
+
+		$("body").on("click", ".addItem", function(e){
+			e.preventDefault();
+			
+  			const selectedRestaurant = $(this).prev().text();
+  			const selectedHTML = `
+  			<div class="listed-names">
+	  			<li class="list-group-item">${selectedRestaurant}<div><input type="checkbox" class="form-check-input" id="exampleCheck1">
+  		  		<label class="form-check-label" for="exampleCheck1"><strong>Visited</strong></label><div></li>
+  		  	</div>	
+  			`
+
+  			$('.list').append(selectedHTML);
+  			restaurantList.push(selectedRestaurant);
+  			console.log(restaurantList.length);
+  				if(restaurantList.length === 10){
+  				$(".addItem").attr("disabled", true);
+  			}
+
+  			
+		})
+
+	$('#clear').on('click', function(e){
+		e.preventDefault();
+		$('.list').empty()
+		restaurantList =[];
+		console.log(restaurantList);
+		$(".addItem").attr("disabled", false);
+
+	})
+
+
+
+
+
+
+	
+
 
 
  
